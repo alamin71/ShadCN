ab321d580f4ac56b434ae7f316369d5614d62326
Al-Amin <alamin50cse.miu@gmail.com>
2025-01-14 16:58:03 +0600

    add

--- src/components/card/Carousel.tsx
+++ src/components/card/Carousel.tsx
@@ -1,37 +1,3 @@
-// import * as React from "react"
-
-// import { Card, CardContent } from "@/components/ui/card"
-// import {
-//   Carousel,
-//   CarouselContent,
-//   CarouselItem,
-//   CarouselNext,
-//   CarouselPrevious,
-// } from "@/components/ui/carousel"
-
-// export function CarouselDemo() {
-//   return (
-//     <Carousel className="w-full max-w-xs">
-//       <CarouselContent>
-//         {Array.from({ length: 5 }).map((_, index) => (
-//           <CarouselItem key={index}>
-//             <div className="p-1">
-//               <Card>
-//                 <CardContent className="flex aspect-square items-center justify-center p-6">
-//                   <span className="text-4xl font-semibold">{index + 1}</span>
-//                 </CardContent>
-//               </Card>
-//             </div>
-//           </CarouselItem>
-//         ))}
-//       </CarouselContent>
-//       <CarouselPrevious />
-//       <CarouselNext />
-//     </Carousel>
-//   )
-// }
-import * as React from "react"
-
 import { Card, CardContent } from "@/components/ui/card"
 import {
   Carousel,
@@ -41,7 +7,7 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel"
 
-// ইমেজগুলো লোকাল ফোল্ডার থেকে ইমপোর্ট করুন
+
 import image1 from "../../assets/1.jpg"
 import image2 from "../../assets/2.jpg"
 import image3 from "../../assets/3.jpg"
@@ -49,7 +15,7 @@ import image4 from "../../assets/4.jpg"
 import image5 from "../../assets/5.jpg"
 
 export function CarouselDemo() {
-  // ইমেজ URL গুলো অ্যারে হিসেবে সংরক্ষণ
+ 
   const images = [image1, image2, image3, image4, image5]
 
   return (
